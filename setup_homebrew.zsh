47f0f5cfe4de75b63a0209dc69ad61026af9357a
pansar1 <7646436+pansar1@users.noreply.github.com>
2021-09-09 18:43:19 +0200

    Add homebew setup to dotbot

---
+++ setup_homebrew.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting Homebrew Setup >>>\n"
+
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Packages
+brew install httpie
+brew install bat
+
+# Applications
+brew install google-chrome
+brew install visual-studio-code
