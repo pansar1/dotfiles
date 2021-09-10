716b7557cbc356117ef7cac04992d392f68e8cea
pansar1 <7646436+pansar1@users.noreply.github.com>
2021-09-10 17:07:35 +0200

    Add global packages

--- setup_node.zsh
+++ setup_node.zsh
@@ -13,3 +13,12 @@ else
     n lts
 fi
 
+# Install Global NPM Packages
+npm install --global firebase-tools
+npm install --global @ionic/cli
+npm install --global typescript
+npm install --global json-server
+npm install --global http-server
+
+echo "Global NPM Packages Installed:"
+npm list --global --depth=0
