7d676992847ab22eaa29d40b6a5efbf62dcd7c89
pansar1 <7646436+pansar1@users.noreply.github.com>
2021-09-11 17:50:47 +0200

    Enforce unique path with a variable

--- setup_node.zsh
+++ setup_node.zsh
@@ -3,7 +3,7 @@
 echo "\n<<< Starting Node Setup >>>\n"
 
 # Node versions are managed with `n`, which is in the Brewfile.
-# See zshrc for N_PREFIX variable and addition to PATH.
+# See zshrc for N_PREFIX variable and addition to path.
 
 if exists node; then
     echo "Node $(node --version) & $(npm --version) already installed"
